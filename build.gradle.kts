2ff7fa4f28cbd595896fe0a8245be5a6d0851deb
Magdalena Rother <magdalena.rother@thermondo.de>
2023-02-22 22:39:28 +0100

    Add pit for mutation testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+    id("info.solidsoft.pitest") version "1.9.0"
 }
 
 group = "com.example"
